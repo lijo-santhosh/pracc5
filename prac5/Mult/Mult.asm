7bbc6c8b1ea6454b3cf54269c2f6062aa7cdb2c4
lijo-santhosh <109999642+lijo-santhosh@users.noreply.github.com>
2023-05-13 21:51:51 +0930

    Update Mult.asm

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,35 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-// Set R0 to 0
+
 @R0
 M=0
 
-// Check if R1 is negative
+
 @R1
 D=M
 @NEGATIVE_CHECK
 D; JLT
 
-// R1 is positive or zero
+
 @LOOP
 0; JMP
 
 (NEGATIVE_CHECK)
-// R1 is negative
+
 @R1
 M=-M
 @R2
 M=-M
 
 (LOOP)
-// Check if R1 is 0
+
 @R1
 D=M
 @END_LOOP
 D; JEQ
 
-// Add R2 to R0
+
 @R0
 D=M
 @R2
@@ -43,11 +43,11 @@ D=D+M
 @R0
 M=D
 
-// Decrement R1
+
 @R1
 M=M-1
 
-// Repeat the loop
+
 @LOOP
 0; JMP
 
